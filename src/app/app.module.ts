88ab09cc6d8ad1f0fc39343f2e37cd4818693c1b
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-04-27 00:24:23 +0530

    [Guru] Created Fundoo Registration page using angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+// import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { RegistrationComponent } from './Components/registration/registration.component';
+import { LoginComponent } from './Components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatCheckboxModule
+    // FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
